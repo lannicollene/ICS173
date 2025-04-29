d1036028c48e82390495e042fb80c402434fce27
lannicollene <57849492+lannicollene@users.noreply.github.com>
2025-04-29 00:47:36 -1000

    copy code from Kaggle Jupyter notebook - plot data using countplot

--- Analysis.py
+++ Analysis.py
@@ -5,3 +5,25 @@ Github Project
 05/04/25
 """
 
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+my_file = "https://raw.githubusercontent.com/lannicollene/ICS173/main/Communication.csv"
+# Load dataset
+df = pd.read_csv(my_file, index_col=0)
+
+# Check the structure
+print(df.head())
+
+sns.countplot(data=df,
+              x="Communication Preference",        
+              hue="Age",                        
+              palette="crest",                    
+              dodge=True)
+
+# save visualization as png
+plt.savefig('visualization/communication_pref_by_age.png')
+
+plt.show()
+
