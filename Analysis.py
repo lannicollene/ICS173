e70e4b6d4a8b069524826bc44586d26d90e0f915
lannicollene <57849492+lannicollene@users.noreply.github.com>
2025-04-29 00:53:04 -1000

    add and fix comments, add 2nd plot with 3 categorical variables

--- Analysis.py
+++ Analysis.py
@@ -9,13 +9,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
+# Path of the .csv file to read
 my_file = "https://raw.githubusercontent.com/lannicollene/ICS173/main/Communication.csv"
-# Load dataset
+
+# Read the file into a variable df
 df = pd.read_csv(my_file, index_col=0)
 
-# Check the structure
+# Check the structure: print the first five rows of the data
 print(df.head())
 
+# Visualize data by creating plot
 sns.countplot(data=df,
               x="Communication Preference",        
               hue="Age",                        
@@ -27,3 +31,20 @@ plt.savefig('visualization/communication_pref_by_age.png')
 
 plt.show()
 
+# Visualize data by creating plot
+comm = sns.catplot(data=df,
+                x="Communication Preference",
+                hue="Gender",
+                col="College Degree", 
+                palette="pastel",
+                kind="count",
+                height=5,
+                aspect=1,
+                dodge=False)
+
+comm.set_titles("College: {col_name}")
+comm.set_axis_labels("Communication Preference", "Count")
+
+# save visualization as png
+plt.savefig('visualization/communication_pref_by_gender_degree.png')
+plt.show()
