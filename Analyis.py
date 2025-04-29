1bda0d8013f17247a8bc5766fc6c96e5ecc81301
lannicollene <57849492+lannicollene@users.noreply.github.com>
2025-04-28 21:33:32 -1000

    load dataset, create data folder

---
+++ Analyis.py
@@ -0,0 +1,16 @@
+
+"""
+Allana Picana
+ICS 173
+Github Project
+05/04/2025
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Load dataset
+df = pd.read_excel('data/Communication_Math115.xlxs')
+
+
